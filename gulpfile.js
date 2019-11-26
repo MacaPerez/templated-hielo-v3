ebdf9d9b48561c3b2feaad1a1cc8043f599c3ae3
macaperez <macaperezcarrillo@gmail.com>
2019-11-26 19:39:43 +0000

    Minifico JS con uglify y concat. Modifico mis documentos html para incluir un único script all.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-    console.log('Hello world!');
+gulp.task('minify', function() {
+  console.log('minifying js ...');
+
+  return gulp.src('src/assets/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/assets/js/'));
 });
