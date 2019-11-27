1a48fe09a49e5a600e24f3066fab8846eedeff3a
macaperez <macaperezcarrillo@gmail.com>
2019-11-27 19:49:24 +0000

    Aprovecho y añado la misma tarea que en la PEC1 para optimizar las imágenes con image-min

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
 
 // Minify JS
 
-gulp.task('minify', function() {
-  console.log('minifying js ...');
+gulp.task('minjs', function() {
+  console.log('minifying js');
 
   return gulp.src('src/js/*.js')
     .pipe(uglify())
@@ -17,7 +18,30 @@ gulp.task('minify', function() {
 // Minify CSS
 
 gulp.task('mincss', () => {
+  console.log('minifying css');
+
   return gulp.src('src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('assets/css/'));
+});
+
+// imagemin
+
+gulp.task('imgmin', () => {
+  console.log('Minify PNG, JPEG, GIF and SVG images');
+
+  return gulp.src('src/images/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+          plugins: [
+              {removeViewBox: true},
+              {cleanupIDs: false}
+          ]
+      })
+
+    ]))
+    .pipe(gulp.dest('images'))
 });
