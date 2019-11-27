87c9691bb2c5a0f45bf9a02da9d2b1cb28cdb811
macaperez <macaperezcarrillo@gmail.com>
2019-11-27 07:58:35 +0000

    Reestructuro directorios src y assets y modifico la tarea en gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('minify', function() {
   console.log('minifying js ...');
 
-  return gulp.src('src/assets/js/*.js')
+  return gulp.src('src/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('build/assets/js/'));
+    .pipe(gulp.dest('assets/js/'));
 });
