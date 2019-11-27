e3a123a6c1a9d1a9a7a724218018b0ed3c5e31a0
macaperez <macaperezcarrillo@gmail.com>
2019-11-27 19:12:31 +0000

    Añado clean CSS y creo la tarea para minificar los archivos CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+
+// Minify JS
 
 gulp.task('minify', function() {
   console.log('minifying js ...');
@@ -8,4 +12,12 @@ gulp.task('minify', function() {
   return gulp.src('src/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('assets/js/'));
+});
+
+// Minify CSS
+
+gulp.task('mincss', () => {
+  return gulp.src('src/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('assets/css/'));
 });
